3f8efd8f40b58bb0a6080d56a1c5d78f9998afd0
Sambit Mishra <labsquantumleap@gmail.com>
2024-07-23 07:52:58 +0530

    Update Argos.py
    Signed-off-by: Sambit Mishra <labsquantumleap@gmail.com>

--- Argos.py
+++ Argos.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
